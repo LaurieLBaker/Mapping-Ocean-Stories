45b391972447da01e8c0b65d40fbd1bf41304802
Laurie Baker <lbaker@coa.edu>
2024-11-04 14:35:40 -0500

    Start WIDS presentation

---
+++ slides/slides/setup.R
@@ -0,0 +1,6 @@
+knitr::opts_chunk$set(cache = TRUE, warning = FALSE, message = FALSE, dpi = 300)
+library(tidyverse)
+#library(silgelib)
+#theme_set(theme_plex())
+my_mirror <- "http://mirrors.xmission.com/gutenberg/"
+doParallel::registerDoParallel()
